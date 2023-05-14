3dc8b82e5d8236ff7bca936d484f8e5a773e4b0e
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2023-05-14 14:39:30 +0600

    Add files via upload

---
+++ mycode22.asm
@@ -0,0 +1,55 @@
+                            .MODEL SMALL  
+ .STACK 100H   
+ .DATA
+   
+   MSG_1  DB  'Enter the character : $'   
+   MSG_2  DB  'Binary form is : $'    
+   MSG_3  DB  'ASCII code are : $'
+   
+     
+  .CODE
+     
+   MAIN PROC      
+   MOV AX, @DATA        
+   MOV DS, AX    
+   LEA DX, MSG_1                     
+   MOV AH, 9      
+   INT 21H       
+   MOV AH, 1
+                            
+   INT 21H      
+   XOR BX, BX                        
+   MOV BL, AL                      
+   LEA DX, MSG_2                 
+   MOV AH, 9      
+   INT 21H       
+   XOR BH, BH                        
+   MOV CX, 8                        
+   MOV AH, 2
+                      
+   OUT:                        
+   SHL BL, 1                        
+   JNC ZZZERO                       
+   INC BH                          
+   MOV DL, 31H                   
+   JMP DISPLAY
+                     
+   ZZZERO:                          
+   MOV DL, 30H
+                        
+   DISPLAY:                          
+   INT 21H                   
+   LOOP OUT                     
+   LEA DX, MSG_3              
+   MOV AH, 9      
+   INT 21H
+         
+   OR BH, 30H                      
+   MOV AH, 2                          
+   MOV DL, BH      
+   INT 21H       
+   MOV AH, 4CH                    
+   INT 21H
+       
+   MAIN ENDP  
+   END MAIN
